508e08a776869757cf6d4ff7bc916f5ab8b2b9d3
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-12-16 10:35:37 +0000

    Better premake file

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,7 @@ project "GLFW"
 
 	kind "StaticLib"
 	language "C"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
@@ -20,9 +21,7 @@ project "GLFW"
 	}
 
 	filter "system:windows"
-		buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
-		staticruntime "On"
 
 		files
 		{
@@ -43,5 +42,11 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
-	filter { "system:windows",  "configurations:Release" }
-		buildoptions "MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
